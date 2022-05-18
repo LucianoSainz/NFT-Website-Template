56d7f8facefe2fb491afe528ac9304e8c2dde732
luciaoSainz <danielsainz28@gmail.com>
2022-05-18 10:46:23 +0200

    worked with contact section

--- src/components/contact.js
+++ src/components/contact.js
@@ -10,15 +10,45 @@ const Contact = () => {
           </div>
           <div class="col-lg-6 contact">
             <form>
-              <input type="text" class="form-control mt-5" id="name"></input>
-              <input type="email" class="form-control mt-4" id="email"></input>
-              <textarea class="form-control mt-4"></textarea>
+              <input
+                type="text"
+                class="form-control mt-5"
+                id="name"
+                placeholder="Name: Raphael Lord"
+              ></input>
+              <input
+                type="email"
+                class="form-control mt-4"
+                id="email"
+                placeholder="Email: raphaelLord@gmail.com"
+              ></input>
+              <textarea
+                rows="5"
+                class="form-control mt-4"
+                placeholder="Messaje: Hi I need more information about the new NFT collection. Please contact me thank you."
+              ></textarea>
               <div class="content-button d-flex justify-content-center">
                 <button type="submit" class="btn btn-primary mt-5">
                   Submit
                 </button>
               </div>
             </form>
+          </div>
+          <div class="col-lg-7 contact-content">
+            <ul class="list-two">
+              <li>
+                Lorem Ipsum is simply dummy text of the printing and
+                typesetting.
+              </li>
+              <li>
+                Lorem Ipsum has been the industry's standard dummy text ever
+                since the 1500s.
+              </li>
+              <li>
+                when an unknown printer took a galley of type and scrambled it
+                to make a type specimen book
+              </li>
+            </ul>
           </div>
         </div>
       </div>
